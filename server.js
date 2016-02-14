033a0b157988f6fb95b5b22347d7300e890c1492
AdamWendell <Adam@websmith.se>
2016-02-14 15:10:43 +0100

    First exercise sulotion fixed

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express'),
+app = express();
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/data', (req, res) => {
+  res.json(jsonData)
+})
+
+app.listen(3000);
